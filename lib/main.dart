1110b1cf620731e2c4b144a2beff26a7ce3527c1
Vitaly Lavrov <lavrovvv@gmail.com>
2021-07-28 23:36:56 +0200

    Move Messaging into separate file

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:zaichat/model/ApplicationState.dart';
-import 'package:zaichat/model/MessageHistoryModel.dart';
 import 'package:zaichat/screens/AuthScreen.dart';
-import 'package:zaichat/util.dart';
+import 'package:zaichat/screens/MessagingScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -49,7 +48,7 @@ class ZaiChatHomePage extends StatelessWidget {
             ),
             body: (){
               if (appState.email != null)
-                return Messaging();
+                return MessagingScreen();
               else
                 return AuthScreen();
             }()
@@ -57,139 +56,3 @@ class ZaiChatHomePage extends StatelessWidget {
       );
     }
   }
-
-  class Messaging extends StatelessWidget {
-
-    @override
-    Widget build(BuildContext context) =>
-      ChangeNotifierProvider(
-        create: (_) => MessageHistoryModel(),
-        child:
-          Center(
-              child: Column(
-                children: <Widget>[
-                  Expanded(
-                      child: MessageHistory()
-                  ),
-                  MessageInput()
-              ],
-            ),
-          )
-    );
-}
-
-class MessageHistory extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) =>
-    Consumer<MessageHistoryModel>(
-      builder: (context, messageHistory, _) =>
-        ListView.separated(
-          separatorBuilder: (context, index) {
-            return Divider();
-          },
-          itemBuilder: (context, index) {
-            final message = messageHistory.getByIndex(index);
-            return Container(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(message.text, key: ValueKey(message.uuid))
-              )
-            );
-          },
-          itemCount: messageHistory.length,
-        )
-    );
-}
-
-class MessageInput extends StatefulWidget {
-
-  @override
-  State createState() {
-    return _MessageInputState();
-  }
-}
-
-class _MessageInputState extends State<MessageInput> {
-
-  final _controller = TextEditingController(text: "");
-
-  final _focusNode = FocusNode();
-
-  var _showSendButton = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller.addListener((){
-      if (_controller.text.isNotEmpty != _showSendButton)
-        setState(() {
-          _showSendButton = _controller.text.isNotEmpty;
-        });
-    });
-  }
-  @override
-  void dispose() {
-    _controller.dispose();
-    _focusNode.dispose();
-    super.dispose();
-  }
-
-  void _sendMessage() {
-    final message = Message(
-      uuid: randomUuid(),
-      text: _controller.text,
-      author: "Vitaly",
-      dateTime: DateTime.now()
-    );
-    // Provider
-    //   .of<MessageHistoryModel>(context, listen: false)
-    //   .addMessage(message);
-    Provider
-        .of<ApplicationState>(context, listen: false)
-        .postMessage(message.text);
-    _controller.text = "";
-    _focusNode.requestFocus();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return
-      Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: <BoxShadow>[
-            BoxShadow(
-              color: Colors.black54,
-              blurRadius: 5,
-              offset: Offset(0.0, 2)
-            )
-          ]
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  autofocus: true,
-                  focusNode: _focusNode,
-                  controller: _controller,
-                  onSubmitted: (_) { _sendMessage(); },
-                  decoration: const InputDecoration(
-                    hintText: 'Message',
-                    border: InputBorder.none,
-                  ),
-                ),
-              ),
-              if (_showSendButton)
-                ElevatedButton(
-                    onPressed: _sendMessage,
-                    child: Text("Send")
-                )
-            ],
-          ),
-        ),
-      );
-  }
-}
